17506d6cf862aef0cea98b6409ab88cd4b446b2f
mervin-njy <mervin.njy@outlook.com>
2023-10-11 12:39:48 +0800

    added colour palette

--- malamola/app/page.tsx
+++ malamola/app/page.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Mala Mola</h1>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title></title>
+      </Head>
+
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
+          <h1>Filly Flower Crafts</h1>
+        </div>
+      </main>
+    </>
   );
 }
--- malamola/tailwind.config.ts
+++ malamola/tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "off-black": "333333",
+        "tealish-blue": "4D6A6D",
+        "accent-orange": "DE8139",
+        "brownish-orange": "CA986C",
+        "off-white": "F3EEE1",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
