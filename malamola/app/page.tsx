7132456bb8e9ffe2e226f9aa7088b779e4ddedff
mervin-njy <mervin.njy@outlook.com>
2023-10-13 21:23:15 +0800

    Displayed main logo on home page, figuring out css

--- malamola/app/page.tsx
+++ malamola/app/page.tsx
@@ -1,22 +1,31 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+import mainLogo from "../public/assets/images/branding/fillyFlowerLogo-1.png";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Head>
-        <title></title>
+        <title>FillyFlower</title>
       </Head>
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>
-          <div>
-            <Link href="/account/login">Log In</Link>
-            <Link href="/account/signup">Sign Up</Link>
-          </div>
-          <h1>Filly Flower Crafts</h1>
+      <main className="flex min-h-screen flex-col items-center p-20">
+        <div className="flex flex-wrap justify-around w-full mb-16">
+          <Link href="/account/login">Log In</Link>
+          <Link href="/account/signup">Sign Up</Link>
+        </div>
+
+        <div className="flex justify-between w-8/12">
+          <Image
+            src={mainLogo}
+            alt="fillyflower's logo"
+            width={400}
+            height={600}
+          />
+          <h1>Welcome to Filly Flower Crafts</h1>
         </div>
       </main>
-    </>
+    </div>
   );
 }
