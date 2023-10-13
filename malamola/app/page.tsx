70fcb7e8e549bf05dbab29b219933302f7f095e1
mervin-njy <mervin.njy@outlook.com>
2023-10-13 17:51:15 +0800

    set up basic main pages w/ app routing & navigating

--- malamola/app/page.tsx
+++ malamola/app/page.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
 
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>
+          <div>
+            <Link href="/account/login">Log In</Link>
+            <Link href="/account/signup">Sign Up</Link>
+          </div>
           <h1>Filly Flower Crafts</h1>
         </div>
       </main>
