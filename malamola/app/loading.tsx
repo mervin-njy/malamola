7c1beeb753608c291039ad68d10f03472d947987
mervin-njy <mervin.njy@outlook.com>
2023-10-24 19:32:28 +0800

    added loading page

---
+++ malamola/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <span className="loading loading-ring loading-lg m-auto mt-32 block text-secondary" />
+  );
+};
+
+export default loading;
