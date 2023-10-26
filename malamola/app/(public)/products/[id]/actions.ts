c90256957edb3f7322efae6cb9eda67a4504115a
mervin-njy <mervin.njy@outlook.com>
2023-10-26 21:06:08 +0800

    added types to cart functions + imported functions at server actions for product/[id] for BtnAddToCart

--- malamola/app/(public)/products/[id]/actions.ts
+++ malamola/app/(public)/products/[id]/actions.ts
@@ -2,6 +2,10 @@
 // server action: Fetch the cart => getCart() || createCart if undefined
 // since the functions are used at different places => they will be created at lib/db
 
-export const incrementProductQuantity = async (productId: string) => {
+import { createCart, getCart } from "@/lib/db/cart";
 
+export const incrementProductQuantity = async (productId: string) => {
+  const cart = (await getCart()) ?? (await createCart()); // creates cart if get cart returns null
+
+  return;
 };
--- malamola/lib/db/cart.ts
+++ malamola/lib/db/cart.ts
@@ -1,10 +1,22 @@
 import { cookies } from "next/dist/client/components/headers";
 import prisma from "./prisma";
+import { Cart, Prisma } from "@prisma/client";
 
-// ShoppingCart type for use at multiple locations
+// 0. export types for use at multiple locations
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  // a. this extends the existing Cart schema w/ the cartItems along w/ product info
+  include: { items: { include: { product: true } } }; // same as cart query at 1b.
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  // b. this extends Cart type in prisma schema (along w/ 0a.) that we will need in 1c.
+  size: number;
+  subtotal: number;
+};
 
 // 1. getCart from cookie (if it exists)
-export const getCart = async () => {
+export const getCart = async (): Promise<ShoppingCart | null> => {
   // a. check if localCart exists in cookie
   const localCartId = cookies().get("localCartId")?.value; // safe call operator since it may be undefined
 
@@ -29,11 +41,17 @@ export const getCart = async () => {
 };
 
 // 2. if doesn't exist, createCart() => which stores newCart in cookie
-export const createCart = async () => {
+export const createCart = async (): Promise<ShoppingCart> => {
   // a. create empty cart w/ no data, but timestamps will be created according to schema
   const newCart = await prisma.cart.create({ data: {} });
 
   // b. store in cookie for reuse => annonymous cart becomes user cart when logged in
   cookies().set("localCartId", newCart.id);
   // TODO: ADD encryption + secure settings for production
+  return {
+    ...newCart,
+    items: [],
+    size: 0,
+    subtotal: 0,
+  };
 };
