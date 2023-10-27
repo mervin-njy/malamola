f2cc173390b1ef66e3634cdd734b1bc33dac2400
mervin-njy <mervin.njy@outlook.com>
2023-10-27 16:59:44 +0800

    added button onclick function to updateProductQuantity with useTransition hook

--- malamola/app/(public)/products/[id]/BtnAddToCart.tsx
+++ malamola/app/(public)/products/[id]/BtnAddToCart.tsx
@@ -1,24 +1,44 @@
 "use client";
-import React from "react";
+import React, { useState, useTransition } from "react";
 import { RiShoppingCartFill } from "react-icons/ri";
 
 interface BtnAddToCartProps {
   productId: string;
+  updateProductQuantity: (productId: string) => Promise<void>;
 }
 
-const BtnAddToCart = ({ productId }: BtnAddToCartProps) => {
+const BtnAddToCart = ({
+  productId,
+  updateProductQuantity,
+}: BtnAddToCartProps) => {
+  // react hooks ---------------------------------------------------------------------------------------------
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState(false);
+
   // event handlers ------------------------------------------------------------------------------------------
   const handleAddToCart = () => {
-    console.log();
+    setSuccess(false);
+    startTransition(async () => {
+      await updateProductQuantity(productId);
+      setSuccess(true);
+    });
   };
 
   // render component ----------------------------------------------------------------------------------------
   return (
     <div className="flex items-center">
-      <button className="btn btn-accent" onClick={handleAddToCart}>
-        Add to Cart
-        <RiShoppingCartFill />
-      </button>
+      {!success && (
+        <button className="btn btn-accent" onClick={handleAddToCart}>
+          Add to Cart
+          <RiShoppingCartFill />
+        </button>
+      )}
+      {isPending && (
+        <span className="loading loading-ring loading-md text-info" />
+      )}
+      {!isPending && success && (
+        <span className="text-success">Added to Cart</span>
+      )}
     </div>
   );
 };
