e772e307388d42dee77751a09122dc9ea07d7fd1
mervin-njy <mervin.njy@outlook.com>
2023-10-31 00:16:47 +0800

    separate DropdownCartcontent from BtnShoppingCart +minor tweaks in css + close btn

--- malamola/app/Navbar/BtnShoppingCart.tsx
+++ malamola/app/Navbar/BtnShoppingCart.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import { ShoppingCart } from "@/lib/db/cart";
-import { formatPrice } from "@/lib/format";
-import Link from "next/link";
 import React from "react";
 import { RiShoppingCart2Fill } from "react-icons/ri";
+import DropdownCart from "./DropdownCart";
 
 // types ----------------------------------------------------------------------------------------------
 interface BtnShoppingCartProps {
@@ -12,12 +9,6 @@ interface BtnShoppingCartProps {
 }
 
 const BtnShoppingCart = ({ cart }: BtnShoppingCartProps) => {
-  // functions ----------------------------------------------------------------------------------------
-  const closeDropdown = () => {
-    const elem = document.activeElement as HTMLElement;
-    if (elem) elem.blur(); // this is important to close BtnshoppingCart dropdown when user is redirected away from the page
-  };
-
   // render component ---------------------------------------------------------------------------------
   return (
     <div className="dropdown dropdown-end">
@@ -34,47 +25,9 @@ const BtnShoppingCart = ({ cart }: BtnShoppingCartProps) => {
       </label>
 
       {/* Dropdown items: Display content of cart */}
-      <div
-        tabIndex={0}
-        className="card dropdown-content card-compact z-30 mt-3 w-72 bg-base-100 shadow-md"
-      >
-        {/* z index to make sure it is on top of the page */}
-        <div className="card-body">
-          <span className="text-lg font-bold">{cart?.size || 0} items</span>
-          {/* Dropdown items in cart */}
-          <ul>
-            {cart?.items.map((item, ind) => {
-              return (
-                <li
-                  key={ind}
-                  className="flex justify-between rounded-md p-1 odd:bg-base-200"
-                >
-                  <span className="flex-1">{item.product.name}</span>
-                  <span className="mr-4">{item.quantity} *</span>
-                  <span>{formatPrice(item.product.price)}</span>
-                </li>
-              );
-            })}
-          </ul>
-
-          <div className="flex justify-between p-1">
-            <span>Subtotal:</span>
-            <span className="text-sm font-semibold">
-              {formatPrice(cart?.subtotal || 0)}
-            </span>
-          </div>
-          {/* Link to /Cart page for full Cart details + checkout confirmation */}
-          <Link
-            href={"/cart"}
-            className="btn btn-accent btn-block"
-            onClick={closeDropdown}
-          >
-            View Cart
-          </Link>
-        </div>
-      </div>
+      <DropdownCart cart={cart} />
     </div>
   );
 };
-// https://drive.google.com/file/d/1q486l56OQ9gYVo9FdrrBTyfsMBw8PFnM/view?usp=sharing
+
 export default BtnShoppingCart;
---
+++ malamola/app/Navbar/DropdownCart.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { ShoppingCart } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/format";
+import Link from "next/link";
+import React from "react";
+
+// types ----------------------------------------------------------------------------------------------
+interface BtnShoppingCartProps {
+  cart: ShoppingCart | null;
+}
+
+const DropdownCart = ({ cart }: BtnShoppingCartProps) => {
+  // functions ----------------------------------------------------------------------------------------
+  const closeDropdown = () => {
+    const elem = document.activeElement as HTMLElement;
+    if (elem) elem.blur(); // this is important to close BtnshoppingCart dropdown when user is redirected away from the page
+  };
+
+  // render component ---------------------------------------------------------------------------------
+  return (
+    <div
+      tabIndex={0}
+      className="card dropdown-content card-compact z-30 mt-3 w-72 bg-base-100 shadow-md"
+    >
+      {/* z index to make sure it is on top of the page */}
+      <div className="card-body">
+        {/* 1. Dropdown header w/ close button */}
+        <div className="flex items-center justify-between text-lg ">
+          <span className="font-bold">{cart?.size || 0} items</span>
+          <div
+            className="cursor-pointer hover:font-black hover:text-accent"
+            onClick={closeDropdown}
+          >
+            x
+          </div>
+        </div>
+
+        {/* 2. Dropdown items in cart */}
+        <ul>
+          {cart?.items.map((item, ind) => {
+            return (
+              <li
+                key={ind}
+                className="flex justify-between rounded-md p-1 italic odd:bg-base-200"
+              >
+                <span className="flex-1">{item.product.name}</span>
+                <span className="mr-4">{item.quantity} x</span>
+                <span>{formatPrice(item.product.price)}</span>
+              </li>
+            );
+          })}
+        </ul>
+
+        {/* 3. Subtotal of cart */}
+        <div className="mt-1 flex justify-between p-1">
+          <span>Subtotal:</span>
+          <span className="text-sm font-semibold">
+            {formatPrice(cart?.subtotal || 0)}
+          </span>
+        </div>
+
+        {/* 4. Link to /Cart page for full Cart details + checkout confirmation */}
+        <Link
+          href={"/cart"}
+          className="btn btn-accent btn-block"
+          onClick={closeDropdown}
+        >
+          View Cart
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default DropdownCart;
