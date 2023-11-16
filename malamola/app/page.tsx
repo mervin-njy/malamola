c68bd2f18f06a5f7deaeeda6627c499ffb3186f8
mervin-njy <mervin.njy@outlook.com>
2023-11-16 22:44:59 +0800

    Minor changes to try deployment again

--- malamola/app/page.tsx
+++ malamola/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import mainLogo from "../public/assets/images/branding/fillyFlowerLogo-1.png";
+// import Image from "next/image";
+// import mainLogo from "../public/assets/images/branding/fillyFlowerLogo-1.png";
 
 export default function Home() {
   return (
@@ -10,14 +10,17 @@ export default function Home() {
         </div> */}
 
       <div className="justify-between">
-        <Image
+        {/* <Image
           src={mainLogo}
           alt="fillyflower's logo"
           width={400}
           height={400}
           className="rounded-full"
-        />
-        <h1>Welcome to Filly Flower Crafts</h1>
+        /> */}
+        <h1 className="text-3xl font-bold tracking-wide">
+          Welcome to Filly Flower Crafts
+        </h1>
+        <p className="mt-4 tracking-wide">Still under construction.</p>
       </div>
     </main>
   );
