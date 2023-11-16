acc522a8206990609eb0cbb4b5ae2766e619e220
mervin-njy <mervin.njy@outlook.com>
2023-11-16 22:49:24 +0800

    attempt to resolve Module not found: Can't resolve deployment

--- malamola/app/page.tsx
+++ malamola/app/page.tsx
@@ -1,5 +1,5 @@
-// import Image from "next/image";
-// import mainLogo from "../public/assets/images/branding/fillyFlowerLogo-1.png";
+import Image from "next/image";
+import mainLogo from "@/public/assets/images/branding/fillyFlowerLogo-1.png";
 
 export default function Home() {
   return (
@@ -10,13 +10,13 @@ export default function Home() {
         </div> */}
 
       <div className="justify-between">
-        {/* <Image
+        <Image
           src={mainLogo}
           alt="fillyflower's logo"
           width={400}
           height={400}
           className="rounded-full"
-        /> */}
+        />
         <h1 className="text-3xl font-bold tracking-wide">
           Welcome to Filly Flower Crafts
         </h1>
