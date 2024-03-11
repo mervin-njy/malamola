5089771de52e435ea2d1636267440da3282ba8f9
mervin-njy <mervin.njy@outlook.com>
2024-03-11 19:09:38 +0800

    updated daisyUI + footer details

--- malamola/app/Footer.tsx
+++ malamola/app/Footer.tsx
@@ -1,27 +1,39 @@
 import React from "react";
+import { AiFillInstagram, AiFillYoutube } from "react-icons/ai";
 
 const Footer = () => {
   // render component ----------------------------------------------------------------------------------------
   return (
     <footer className="border-t-2 border-accent border-opacity-10 bg-neutral bg-opacity-30 p-10 text-neutral-content shadow-sm">
-      <div className="footer m-auto max-w-7xl">
-        <div>
-          <span className="footer-title">Services</span>
-          <a className="link-hover link">Design</a>
-          <a className="link-hover link">Education</a>
-          <a className="link-hover link">Marketing</a>
+      <div className="m-auto max-w-7xl">
+        {/* Link to affiliated sites */}
+        <div className="flex gap-2">
+          <a
+            className="link-hover link text-secondary hover:text-accent"
+            href="https://instagram.com/fillyflower_crafts/"
+          >
+            <AiFillInstagram size={30} />
+          </a>
+
+          {/* TO BE IMPLEMENTED */}
+          <a className="link-hover link text-secondary hover:text-accent">
+            <AiFillYoutube size={30} />
+          </a>
         </div>
+
+        <div className="divider" />
+
+        {/* Link to developer */}
         <div>
-          <span className="footer-title">About us</span>
-          <a className="link-hover link">Contact</a>
-          <a className="link-hover link">Enquiries</a>
-          <a className="link-hover link">Collaborations</a>
-        </div>
-        <div>
-          <span className="footer-title">Legal</span>
-          <a className="link-hover link">Terms of use</a>
-          <a className="link-hover link">Privacy policy</a>
-          <a className="link-hover link">Cookie policy</a>
+          <span className="text-xs italic">
+            Designed and built by{" "}
+            <a
+              className="link-hover link font-semibold text-secondary"
+              href="https://github.com/mervin-njy"
+            >
+              mervin-njy.
+            </a>
+          </span>
         </div>
       </div>
     </footer>
