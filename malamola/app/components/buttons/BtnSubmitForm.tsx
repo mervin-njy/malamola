6e957b6a422a5d52bed9ed548eb151d5d0831f13
mervin-njy <mervin.njy@outlook.com>
2024-04-08 19:33:27 +0800

    Removed experimental_useFormStatus
    Forgot to remove in previous commit

--- malamola/app/components/buttons/BtnSubmitForm.tsx
+++ malamola/app/components/buttons/BtnSubmitForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { ComponentProps } from "react";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 type BtnSubmitFormProps = {
   children: React.ReactNode;
